d8975a2172838ffc44c99cbd3b29f5fe010959a9
Marco Velasco <marcovelas71@gmail.com>
2024-09-02 11:38:20 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# p1_donut_app_velasco_marco
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/p1_donut_app_velasco_marco/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p1_donut_app_velasco_marco
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
