ce60a40e502a71ff871a57cfcf41984af12f92ec
Marco Velasco <marcovelas71@gmail.com>
2024-09-09 13:17:44 -0600

    mytab añadido

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../utils/my_tab.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -8,13 +10,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Widget> myTabs = [
+    //donut tab
+    const MyTab(
+      iconPath: 'lib/icons/donut.png',
+    ),
+    //burguer tab
+    const MyTab(
+      iconPath: 'lib/icons/burger.png',
+    ),
+    //smoti
+    const MyTab(
+      iconPath: 'lib/icons/smoothie.png',
+    ),
+    //pancakeaqe
+    const MyTab(
+      iconPath: 'lib/icons/pancakes.png',
+    ),
+    //pizza
+    const MyTab(
+      iconPath: 'lib/icons/pizza.png',
+    )
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: const Color.fromARGB(255, 10, 134, 236),
-          leading: const Icon(Icons.menu,
-              color: Color.fromARGB(255, 255, 248, 248))),
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Icon(Icons.menu, color: Colors.grey[800]),
+          ),
+          actions: const [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0),
+              child: Icon(Icons.person),
+            )
+          ],
+        ),
+        body: Column(children: [
+          const Padding(
+            padding: EdgeInsets.all(24.0),
+            child: Row(
+              children: [
+                Text(
+                  "I want to ",
+                  style: TextStyle(
+                    fontSize: 24,
+                  ),
+                ),
+                Text(
+                  "Eat",
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      decoration: TextDecoration.underline),
+                ),
+              ],
+            ),
+          ), //i want to eat
+          TabBar(tabs: myTabs) //tab barr
+          //
+        ]),
+      ),
     );
   }
 }
